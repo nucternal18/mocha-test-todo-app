fcfe7fcc017a318371aca3642775d48dd31df2ae
A Oladipupo-Usoro <aolausoro@gmail.com>
2021-12-03 23:03:06 +0000

    added and configured jest eslint test-library/react lint-staged

---
+++ jest.config.js
@@ -0,0 +1,33 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  clearMocks: true,
+  coverageDirectory: "coverage",
+  collectCoverageFrom: [
+    "**/*.{js,jsx,ts,tsx}",
+    "!**/*.d.ts",
+    "!**/node_modules/**",
+  ],
+  moduleNameMapper: {
+    /* Handle image imports
+    https://jestjs.io/docs/webpack#handling-static-assets */
+    "^.+\\.(jpg|jpeg|png|gif|webp|avif|svg)$":
+      "<rootDir>/__mocks__/fileMock.js",
+  },
+  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/.next/"],
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  globals: {
+    "ts-jest": {
+      tsconfig: "tsconfig.jest.json",
+    },
+  },
+  setupFilesAfterEnv: ["./jest.setup.ts"],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+};
